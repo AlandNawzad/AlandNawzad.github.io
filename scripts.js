93460aae469f2541f32e7d0391092cb7251add77
Aland Nawzad <alandnawzad2023@gmail.com>
2024-08-12 18:52:54 +0300

    add new project to the project section and fix some responsive issues

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu= document.querySelector(".humberger-icon");
+    const icon= document.querySelector(".menu-links");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    
+}
